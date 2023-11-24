6c77b855f69fa8984faf3e8143ae192c174960fd
Дмитрий Рябков <demitry@e1.ru>
2023-11-24 17:38:34 +0500

    Profile views and serializers updated.

--- creating_and_editing_a_resume/api/serializers.py
+++ creating_and_editing_a_resume/api/serializers.py
@@ -1,10 +1,71 @@
 from rest_framework import serializers
+from user.models.education import Education
 from user.models.user import ResumeUser
+from user.models.work import EmploymentHistory
+
+
+class EducationSerializer(serializers.ModelSerializer):
+    """Сериализатор для образования."""
+
+    class Meta:
+        model = Education
+        fields = (
+            "university",
+            "start_date",
+            "end_date",
+            "speciality",
+            "grade",
+        )
+
+
+class EmploymentHistorySerializer(serializers.ModelSerializer):
+    """Сериализатор для опыта работы."""
+
+    class Meta:
+        model = EmploymentHistory
+        fields = (
+            "company",
+            "web_page",
+            "position",
+            "start_date",
+            "end_date",
+            "responsibilities",
+        )
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Сериализатор для личного кабинета пользователя."""
 
+    education = EducationSerializer(many=True, required=False)
+    work = EmploymentHistorySerializer(many=True, required=False)
+
     class Meta:
         model = ResumeUser
-        fields = "__all__"
+        fields = (
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "phone",
+            "telegram",
+            "city",
+            "birth_date",
+            "education",
+            "work",
+        )
+
+
+class ShortProfileSerializer(ProfileSerializer):
+    """Короткий сериализатор для личного кабинета пользователя."""
+
+    class Meta(ProfileSerializer.Meta):
+        fields = (
+            "first_name",
+            "last_name",
+            "phone",
+            "telegram",
+            "city",
+            "birth_date",
+            "education",
+            "work",
+        )
